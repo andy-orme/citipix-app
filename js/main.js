7b013f2b80267b82e0d2116b6e2ffa30a12e6c4d
Andy Orme <andy@pixl8.co.uk>
2018-03-24 14:57:21 +0000

    Set-up conditional statement for different inputs
    > Set-up based on initial assumtion, seems to only set background to NYC example. Needs a re-think...

--- js/main.js
+++ js/main.js
@@ -4,35 +4,38 @@ $(function() {
     // preventDefault behaviour of the "update" button
     e.preventDefault();
     // store user input to "var = city"
-      // `$.val();`
-      // `$.toLowerCase();`
-      // `console.log(city);` to test effectiveness
-    var city = $("#city-type").val().toLowerCase();
-    console.log(city);
+    var city = $("#city-type").val().toLowerCase(); // collect the input value and make is lowercase
 
     // set up conditional statement
-
-      // if (city "new york"  || "new york city" || "nyc")
-
+      // if (city "new york" || "new york city" || "nyc")
+      if (city === "new york" || "new york city" || "nyc") {
         // use `$.attr();` and change background-img to "../images/nyc.jpg"
-
-      // else if (city "san francisco"  || "sf" || "bay area")
-      
+        $("body").attr("class", "nyc");
+      }
+      // else if (city "san francisco" || "sf" || "bay area")
+      else if (city === "san francisco" || "sf" || "bay area") {
         // use `$.attr();` and change background-img to "../images/sf.jpg"
-
-      // else if (city "los angeles"  || "la" || "lax")
-      
+        $("body").attr("class", "sf");
+      }
+      // else if (city "los angeles" || "la" || "lax")
+      else if (city === "los angeles" || "la" || "lax") {
         // use `$.attr();` and change background-img to "../images/la.jpg"
-
-      // else if (city "austin"  || "atx")
-      
+        $("body").attr("class", "la");
+      }
+      // else if (city "austin" || "atx")
+      else if (city === "austin" || "atx") {
         // use `$.attr();` and change background-img to "../images/austin.jpg"
-
-      // else if (city "sydney"  || "syd")
-      
+        $("body").attr("class", "austin");
+      }
+      // else if (city "sydney" || "syd")
+      else if (city === "sydney" || "syd") {
         // use `$.attr();` and change background-img to "../images/sydney.jpg"
-
+        $("body").attr("class", "sydney");
+      }
       // else do nothing (maybe a message?)
+      else {
+        $("body").attr();
+      }
 
   });    
 });
