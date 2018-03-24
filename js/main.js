7432d60286c353cda84c3e6ca4178c1cfd56aa0a
Andy Orme <andy@pixl8.co.uk>
2018-03-24 12:52:21 +0000

    Linked up js files and set-up basic pseudo code
    > Linked up js files in the index.html
    > Set-up basic pseudo code in main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function() {
+  // event listener on the "update" button
+    
+    // preventDefault behaviour of the "update" button
+
+    // store user input to "var = city"
+
+      // `$.val();`
+
+      // `$.toLowerCase();`
+
+    // `console.log(city);` to test effectiveness
+
+    // set up conditional statement
+
+      // if (city "new york"  || "new york city" || "nyc")
+
+        // use `$.attr();` and change background-img to "../images/nyc.jpg"
+
+      // else if (city "san francisco"  || "sf" || "bay area")
+      
+        // use `$.attr();` and change background-img to "../images/sf.jpg"
+
+      // else if (city "los angeles"  || "la" || "lax")
+      
+        // use `$.attr();` and change background-img to "../images/la.jpg"
+
+      // else if (city "austin"  || "atx")
+      
+        // use `$.attr();` and change background-img to "../images/austin.jpg"
+
+      // else if (city "sydney"  || "syd")
+      
+        // use `$.attr();` and change background-img to "../images/sydney.jpg"
+
+      // else do nothing (maybe a message?)
+
+});
