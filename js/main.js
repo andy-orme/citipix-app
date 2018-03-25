9d1ab5622bc261dda4716e213cd4602492e371a2
Andy Orme <andy@pixl8.co.uk>
2018-03-25 12:21:29 +0100

    Reset value on submission
    > Reset "#city-type" field on submission setting the val to null

--- js/main.js
+++ js/main.js
@@ -3,33 +3,35 @@ $(function() {
   $("#submit-btn").on("click", function(e) {
     // preventDefault behaviour of the "update" button
     e.preventDefault();
+
     // store user input to "var = city"
     var city = $("#city-type").val().toLowerCase(); // collect the input value and makes it lowercase
     
     // set up conditional statement
-      // if (city "new york" || "new york city" || "nyc")
-      if (city === "new york" || city === "new york city" || city === "nyc") {
-        $("body").attr("class", "nyc"); // use `$.attr();` and change background-img to "../images/nyc.jpg"
-      }
-      // else if (city "san francisco" || "sf" || "bay area")
-      else if (city === "san francisco" || city === "sf" || city === "bay area") {
-        $("body").attr("class", "sf"); // use `$.attr();` and change background-img to "../images/sf.jpg"
-      }
-      // else if (city "los angeles" || "la" || "lax")
-      else if (city === "los angeles" || city === "la" || city === "lax") {
-        $("body").attr("class", "la"); // use `$.attr();` and change background-img to "../images/la.jpg"
-      }
-      // else if (city "austin" || "atx")
-      else if (city === "austin" || city === "atx") {
-        $("body").attr("class", "austin"); // use `$.attr();` and change background-img to "../images/austin.jpg"
-      }
-      // else if (city "sydney" || "syd")
-      else if (city === "sydney" || city === "syd") {
-        $("body").attr("class", "sydney"); // use `$.attr();` and change background-img to "../images/sydney.jpg"
-      }
-      else {
-        $("body").attr(); // else do nothing (maybe a message?)
-      }
+    // if (city "new york" || "new york city" || "nyc")
+    if (city === "new york" || city === "new york city" || city === "nyc") {
+      $("body").attr("class", "nyc"); // use `$.attr();` and change background-img to "../images/nyc.jpg"
+    }
+    // else if (city "san francisco" || "sf" || "bay area")
+    else if (city === "san francisco" || city === "sf" || city === "bay area") {
+      $("body").attr("class", "sf"); // use `$.attr();` and change background-img to "../images/sf.jpg"
+    }
+    // else if (city "los angeles" || "la" || "lax")
+    else if (city === "los angeles" || city === "la" || city === "lax") {
+      $("body").attr("class", "la"); // use `$.attr();` and change background-img to "../images/la.jpg"
+    }
+    // else if (city "austin" || "atx")
+    else if (city === "austin" || city === "atx") {
+      $("body").attr("class", "austin"); // use `$.attr();` and change background-img to "../images/austin.jpg"
+    }
+    // else if (city "sydney" || "syd")
+    else if (city === "sydney" || city === "syd") {
+      $("body").attr("class", "sydney"); // use `$.attr();` and change background-img to "../images/sydney.jpg"
+    }
+    else {
+      $("body").attr(""); // else do nothing (maybe a message?)
+    }
 
-  });    
+    $("#city-type").val(null);
+  });
 });
